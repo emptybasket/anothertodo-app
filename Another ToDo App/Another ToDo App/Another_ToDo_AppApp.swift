acc67ab021b87b8b929b77c10778e01fe5f3ecf4
Joash Tubaga <joash.tubaga@codewithchris.com>
2023-06-20 22:58:17 +0800

    Initial Commit
    Add Project File

---
+++ Another ToDo App/Another ToDo App/Another_ToDo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Another_ToDo_AppApp.swift
+//  Another ToDo App
+//
+//  Created by Joash Tubaga on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Another_ToDo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Another ToDo App/Another ToDo App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Another ToDo App
+//
+//  Created by Joash Tubaga on 6/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
